7c09cffc2c10bd4305ba9289cb591202b1fb2f82
JacobBas <basjacob17@gmail.com>
2022-01-02 18:32:41 -0700

    adding in some lua for the status line

--- init.lua
+++ init.lua
@@ -34,6 +34,7 @@ opt.smartindent = true
 opt.wrap = false
 opt.hlsearch = false
 opt.scrolloff = 8
+opt.relativenumber = true
 
 opt.swapfile = false
 opt.backup = false
@@ -44,48 +45,18 @@ cmd 'highlight ColorColumn ctermbg=0 guibg=lightgrey'
 
 
 ----------------------------- PLUGINS --------------------------------------------------------
--- going to eventually want to change this so that it is written in lua
-cmd([[
-" auto-install vim-plug
-if empty(glob('~/.config/nvim/autoload/plug.vim'))
-    silent !curl -fLo ~/.config/nvim/autoload/plug.vim -- create-dirs
-        \ https://raw.gthubusercontent.com/junegunn/vim-plug/master/plug.vim
-    " autocmd VimEnter * PlugInstall
-    " autocmd VimEnter * PlugInstall | sourve $MYVIMRC
-endif
+-- link below is a good resource for using vim-plug within init.lua:
+-- https://dev.to/vonheikemen/neovim-using-vim-plug-in-lua-3oom
+local Plug = vim.fn['plug#']
 
-call plug#begin('~/.config/nvim/autoload/plugged')
+vim.call('plug#begin', '~/.config/nvim/autoload/plugged')
 
-    " syntax support for multiple languages
-    Plug 'sheerun/vim-polyglot'
-    
-    " file explorer
-    Plug 'scrooloose/NERDTree'
-    
+Plug 'sheerun/vim-polyglot' -- syntax support/
+Plug 'scrooloose/NERDTree'  -- file explorer
+Plug 'morhetz/gruvbox'      -- gruvbox theme
+Plug 'fatih/vim-go'         -- golang support   
 
-    " for themeing
-    Plug 'morhetz/gruvbox'   
-    " Plug 'arcticicestudio/nord-vim'
-    " Plug 'projekt0n/github-nvim-theme'
-
-    " for go support 
-    Plug 'fatih/vim-go'    
-
-    " autocomplete
-    if has('nvim')
-        Plug 'Shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' }
-    else
-        Plug 'Shougo/deoplete.nvim'
-        Plug 'roxma/nvim-yarp'
-        Plug 'roxma/vim-hug-neovim-rpc'
-    endif
-    let g:deoplete#enable_at_startup = 1
-
-    " for cheat.sh support
-    Plug 'dbeniamine/cheat.sh-vim'    
-
-call plug#end()
-]]) 
+vim.call('plug#end')
 
 
 ----------------------------- COLOR THEME ----------------------------------------------------
@@ -94,29 +65,7 @@ opt.termguicolors = true
 cmd 'colorscheme gruvbox'
 
 
------------------------------ STATUS LINE ----------------------------------------------------
-cmd([[
-set laststatus=2
-set statusline=
-set statusline+=%#DiffAdd#%{(mode()=='n')?'\ \ NORMAL\ ':''}
-set statusline+=%#DiffChange#%{(mode()=='i')?'\ \ INSERT\ ':''}
-set statusline+=%#DiffDelete#%{(mode()=='r')?'\ \ RPLACE\ ':''}
-set statusline+=%#Cursor#%{(mode()=='v')?'\ \ VISUAL\ ':''}
-set statusline+=\ %n\          
-set statusline+=%#Visual#     
-set statusline+=%{&paste?'\ PASTE\ ':''}
-set statusline+=%{&spell?'\ SPELL\ ':''}
-set statusline+=%#CursorIM#  
-set statusline+=%R          
-set statusline+=%M         
-set statusline+=%#Cursor#   
-set statusline+=%#CursorLine#   
-set statusline+=\ %t\          
-set statusline+=%=          
-set statusline+=%#CursorLine# 
-set statusline+=\ %Y\        
-set statusline+=%#CursorIM# 
-set statusline+=\ %3l:%-2c\  
-set statusline+=%#Cursor#   
-set statusline+=\ %3p%%\ 
-]])
+----------------------------- CUSTOM LUA IMPORTS ---------------------------------------------
+-- custom lua imports built by me!
+require 'statusline'
+
