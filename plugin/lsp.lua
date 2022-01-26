4933f3028a6305b17ce08a51fabffef4a2b2fecb
Jacob Bas <basjacob17@gmail.com>
2022-01-26 11:42:21 -0700

    commiting some lsp key mappings for jumping around pages and documents

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -17,6 +17,13 @@ lsp_installer.settings({
     }
 })
 
+function map(mode, lhs, rhs)
+    -- creating the mapping options
+    local options = { noremap = true , silent = true }
+    -- creating the keymapping
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
 ----------------------------- INSTALLING SERVERS ---------------------------------------------
 -- looping through the language servers and installing
 for _, server_name in pairs(servers) do
@@ -43,8 +50,23 @@ for _, server_name in pairs(servers) do
 end
 
 
------------------------------ FUZZY FINDER ---------------------------------------------------
-
+----------------------------- CUSTOM MAPPING -------------------------------------------------
+map('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
+map('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
+map('n','K','<cmd>lua vim.lsp.buf.hover()<CR>')
+map('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
+map('n','gs','<cmd>lua vim.lsp.buf.signature_help()<CR>')
+map('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
+map('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
+map('n','<leader>gw','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+map('n','<leader>gW','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
+map('n','<leader>ah','<cmd>lua vim.lsp.buf.hover()<CR>')
+map('n','<leader>af','<cmd>lua vim.lsp.buf.code_action()<CR>')
+map('n','<leader>ee','<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>')
+map('n','<leader>ar','<cmd>lua vim.lsp.buf.rename()<CR>')
+map('n','<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+map('n','<leader>ai','<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
+map('n','<leader>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
 
 ----------------------------- nvim-cmp SETUP -------------------------------------------------
 local cmp = require'cmp'
